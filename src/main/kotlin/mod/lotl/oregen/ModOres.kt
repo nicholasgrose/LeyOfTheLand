483eaaa661f0f1ebac896d4bc9ae2af0a2f3a5f1
Epicjii <jacobliggett2000@gmail.com>
2020-12-17 19:26:34 -0500

    Added enlighten logs into appropriate tags

--- src/main/kotlin/mod/lotl/oregen/ModOres.kt
+++ src/main/kotlin/mod/lotl/oregen/ModOres.kt
@@ -2,12 +2,21 @@ package mod.lotl.oregen
 
 import mod.lotl.LeyOfTheLand
 import mod.lotl.block.ModBlocks
+import net.minecraft.util.registry.Registry
+import net.minecraft.util.registry.WorldGenRegistries
+import net.minecraft.world.biome.Biome
+import net.minecraft.world.gen.GenerationStage
 import net.minecraft.world.gen.feature.ConfiguredFeature
 import net.minecraft.world.gen.feature.Feature
 import net.minecraft.world.gen.feature.OreFeatureConfig
 import net.minecraft.world.gen.placement.*
+import net.minecraftforge.event.world.BiomeLoadingEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
+import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.registries.ForgeRegistries
+import thedarkcolour.kotlinforforge.forge.FORGE_BUS
 import thedarkcolour.kotlinforforge.forge.KDeferredRegister
+import thedarkcolour.kotlinforforge.forge.MOD_BUS
 
 object ModOres {
     // use of the new KDeferredRegister
@@ -19,12 +28,21 @@ object ModOres {
                 OreFeatureConfig(
                     OreFeatureConfig.FillerBlockType.BASE_STONE_OVERWORLD,
                     ModBlocks.CRYSTALLIZED_LEY_ORE.defaultState,
-                    5
+                    4
                 )
             )
             .withPlacement(
-                ConfiguredPlacement(Placement.DEPTH_AVERAGE, DepthAverageConfig(10, 3))
+                ConfiguredPlacement(Placement.DEPTH_AVERAGE, DepthAverageConfig(8, 5))
             )
     }
 }
 
+object ModOreLoader {
+    @SubscribeEvent
+    fun onBiomeLoading(event: BiomeLoadingEvent) {
+        if (event.category == Biome.Category.NETHER || event.category == Biome.Category.THEEND) return
+
+        event.generation.getFeatures(GenerationStage.Decoration.UNDERGROUND_ORES)
+            .add { ModOres.CRYSTALLIZED_LEY_ORE }
+    }
+}
